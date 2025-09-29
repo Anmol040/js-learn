c7a9d4a93b79545c52dd49cd4b45218b83d96b74
ANMOL BHARDWAJ <100508530+Anmol040@users.noreply.github.com>
2025-09-29 10:13:59 +0000

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Anmol Bhardwaj"; // string
+let age = 20; // number
+let isLoggedIn = false; // boolean
+
+let state = null; // empty value
+let city= 
+console.log(typeof isLoggedIn);
+console.log(city);undefined; // value not assigned yet
