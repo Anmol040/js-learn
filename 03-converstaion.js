f8c2290343512e827d751598b239d28359ea1e95
ANMOL BHARDWAJ <100508530+Anmol040@users.noreply.github.com>
2025-09-29 11:51:42 +0000

    conversation of data types

---
+++ 03-converstaion.js
@@ -0,0 +1,17 @@
+let score = "33" 
+
+console.log(typeof score);// 
+console.log(typeof(score)); //
+
+// if we want to do operaton on only number tahm...
+
+let valueInNumber = Number(score); // convert string to number
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // NaN -> not a number
+
+
+let somenumber = 1234
+
+let stringNumber = String(somenumber); // convert number to string
+console.log(typeof stringNumber);
+console.log(stringNumber); // 1234 
