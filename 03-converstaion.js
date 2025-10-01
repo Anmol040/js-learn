5f16290c2bcd2b9a508b458bb5090dd028515823
ANMOL BHARDWAJ <100508530+Anmol040@users.noreply.github.com>
2025-10-01 10:30:24 +0000

    data summary

--- 03-converstaion.js
+++ 03-converstaion.js
@@ -10,8 +10,35 @@ console.log(typeof valueInNumber);
 console.log(valueInNumber); // NaN -> not a number
 
 
-let somenumber = 1234
+let somenumber1 = 1234
 
-let stringNumber = String(somenumber); // convert number to string
+let stringNumber = String(somenumber1); // convert number to string
 console.log(typeof stringNumber);
 console.log(stringNumber); // 1234 
+
+
+let somenumber = 1234
+
+let stringNumber1 = String(somenumber); // convert number to string
+console.log(typeof stringNumber1);
+console.log(stringNumber1); // 1234
+
+// **************************Operations in js****************************
+
+let value = 3
+let negValue = -value
+console.log(negValue); // -3
+
+
+let str1 = "Hello"
+let str2 = " Anmol"
+let str3 = str1 + str2 // concatenation
+console.log(str3); // Hello Anmol
+
+console.log("1"+ 5);
+console.log("1"+ "5");
+console.log(1 + 5 + "5");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // 101
---
+++ 04-datatype-summary.js
@@ -0,0 +1,23 @@
+// # Primitive Data Types
+
+// 7 types of data types in JS which are primitive data types
+//  example 
+// 1. String, 2. Number, 3. Boolean, 4. null, 5. undefined, 6. Symbol, 7. BigInt
+
+// Reference Data Types (non-primitive data types)
+
+// Array, Object literals, Functions, Dates, etc'
+
+const heros = ["shaktiman", "naagraj", "doga"]; // array
+let myObj = {        // object is always in curly braces ,  object is key value pair  
+    name: "Anmol",
+    age: 24,
+}
+
+const myFunction = function() {  // function
+    console.log("hello world");
+}
+console.log(typeof myFunction); // function
+console.log(myFunction); // [Function: myFunction]
+
+
